3923a5165bbea222089c5aafe644b5eee93a852a
Ajay <ajay@aipopuli.com>
2022-06-24 22:25:28 +0530

    test

---
+++ database/migrations/2022_06_24_135630_create_marks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('student_id');
+            $table->string('maths');
+            $table->string('science');
+            $table->string('history');
+            $table->string('term');
+            $table->tinyInteger('total_marks');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
